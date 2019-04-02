5453ec8f4101d1f04d9da921d9cb4f16b382f568
ZhangWYy <1109827726@qq.com>
2019-04-03 00:43:08 +0800

    indicatorB

--- app/src/main/java/first/swufe/com/sorce/IndicatorB.java
+++ app/src/main/java/first/swufe/com/sorce/IndicatorB.java
@@ -10,10 +10,14 @@ public class IndicatorB extends AppCompatActivity implements View.OnClickListene
 
     TextView scoreA;
     TextView scoreB;
+    Button add3A;
+    Button add2A;
+    Button add1A;
+    Button add3B;
+    Button add2B;
+    Button add1B;
     Button reset;
     int a=0,b=0;
-    String ss=null;
-    String s=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +28,7 @@ public class IndicatorB extends AppCompatActivity implements View.OnClickListene
             public void onClick(View v) {
                 if(v.getId()==R.id.add3A){
                     a=a+3;
-                    ss = Integer.toString(a);
-                    scoreA.setText(ss);
+                    scoreA.setText(Integer.toString(a));
                 }
             }
         });
@@ -34,8 +37,7 @@ public class IndicatorB extends AppCompatActivity implements View.OnClickListene
             public void onClick(View v) {
                 if(v.getId()==R.id.add2A){
                     a=a+2;
-                    ss = Integer.toString(a);
-                    scoreA.setText(ss);
+                    scoreA.setText(Integer.toString(a));
                 }
             }
         });
@@ -44,8 +46,7 @@ public class IndicatorB extends AppCompatActivity implements View.OnClickListene
             public void onClick(View v) {
                 if(v.getId()==R.id.add1A){
                     a=a+1;
-                    ss = Integer.toString(a);
-                    scoreA.setText(ss);
+                    scoreA.setText(Integer.toString(a));
                 }
             }
         });
@@ -53,45 +54,40 @@ public class IndicatorB extends AppCompatActivity implements View.OnClickListene
         scoreB = findViewById(R.id.scoreB);
         findViewById(R.id.add3B).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if(v.getId()==R.id.add3B){
+            public void onClick(View w) {
+                if(w.getId()==R.id.add3B){
                     b=b+3;
-                    s = Integer.toString(b);
-                    scoreB.setText(s);
+                    scoreB.setText(Integer.toString(b));
                 }
             }
         });
         findViewById(R.id.add2B).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if(v.getId()==R.id.add2B){
+            public void onClick(View w) {
+                if(w.getId()==R.id.add2B){
                     b=b+2;
-                    s = Integer.toString(b);
-                    scoreB.setText(s);
+                    scoreB.setText(Integer.toString(b));
                 }
             }
         });
         findViewById(R.id.add1B).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if(v.getId()==R.id.add1B){
+            public void onClick(View w) {
+                if(w.getId()==R.id.add1B){
                     b=b+1;
-                    s = Integer.toString(b);
-                    scoreB.setText(s);
+                    scoreB.setText(Integer.toString(b));
                 }
             }
         });
 
         findViewById(R.id.Reset).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if (v.getId() == R.id.Reset) {
+            public void onClick(View w) {
+                if (w.getId() == R.id.Reset) {
                     a = 0;
                     b=0;
-                    ss = Integer.toString(a);
-                    s = Integer.toString(b);
-                    scoreA.setText(ss);
-                    scoreB.setText(s);
+                    scoreA.setText(Integer.toString(a));
+                    scoreB.setText(Integer.toString(b));
                 }
             }
         });
